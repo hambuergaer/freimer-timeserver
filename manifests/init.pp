96758fba3d1f3093f13944d04a2f38487e5aab10
Frank Reimer <frank.reimer@kuehne-nagel.com>
2016-03-31 09:25:12 +0200

    Removed chrony for RHEL 7 because IPA client depends on ntp on RHEL 7 instead of chrony.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,38 +3,39 @@
 class timeserver (
 $ntpServers = ['de.pool.ntp.org'] )
 {
-  if $operatingsystem == 'RedHat' and $operatingsystemmajrelease == 7 {
-    package { 'ntp':
-      ensure => 'purged',
-    }
-    package { 'chrony':
-      ensure => 'installed',
-    }
-    service { 'ntpd':
-      ensure => 'stopped',
-      enable => false,
-    }
-    service { 'chronyd':
-      ensure     => 'running',
-      enable     => true,
-      require    => Package['chrony'],
-      hasstatus  => true,
-      hasrestart => true,
-    }
-    file { '/etc/chrony.conf':
-      ensure  => file,
-      content => template('timeserver/chrony.erb'),
-      require => Package['chrony'],
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      notify  => Service['chronyd'],
-    }
-    
-    
-  }
+#  if $operatingsystem == 'RedHat' and $operatingsystemmajrelease == 7 {
+#    package { 'ntp':
+#      ensure => 'purged',
+#    }
+#    package { 'chrony':
+#      ensure => 'installed',
+#    }
+#    service { 'ntpd':
+#      ensure => 'stopped',
+#      enable => false,
+#    }
+#    service { 'chronyd':
+#      ensure     => 'running',
+#      enable     => true,
+#      require    => Package['chrony'],
+#      hasstatus  => true,
+#      hasrestart => true,
+#    }
+#    file { '/etc/chrony.conf':
+#      ensure  => file,
+#      content => template('timeserver/chrony.erb'),
+#      require => Package['chrony'],
+#      owner   => 'root',
+#      group   => 'root',
+#      mode    => '0644',
+#      notify  => Service['chronyd'],
+#    }
+#    
+#    
+#  }
 
-  if $operatingsystem == 'RedHat' and $operatingsystemmajrelease == 6 {
+  #if $operatingsystem == 'RedHat' and $operatingsystemmajrelease == 6 {
+  if $operatingsystem == 'RedHat' or  $operatingsystem == 'CentOS'{
     package { 'ntp':
       ensure => installed,
     }
